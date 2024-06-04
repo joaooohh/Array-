3cbc434f294361fc5164da02c703ec15c673057e
joaooohh <140817126+joaooohh@users.noreply.github.com>
2024-06-04 11:03:35 -0300

    Add files via upload

---
+++ 3-array-pop-shift.js
@@ -0,0 +1,9 @@
+var frutas = ['Maçã', 'Banana', 'Laranja', 'Pêssego', 'Abacaxi'];
+
+var ultimaFruta = frutas.pop();
+console.log('Array após remover o útimo elemento:', frutas);
+console.log('Ùltimo fruta removida:', ultimaFruta); 
+
+var primeiraFruta = frutas.shift();
+console.log('Array após remover o primeiro elemento:', frutas);
+console.log('Primeira fruta removida:', primeiraFruta);
