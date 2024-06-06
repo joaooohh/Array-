df1d2095ac64a7ae207a4f23318fc55cf21b6321
joaooohh <140817126+joaooohh@users.noreply.github.com>
2024-06-06 10:54:53 -0300

    Add files via upload

---
+++ 7-dividindo-sala.js
@@ -0,0 +1,29 @@
+const alunos = [
+ "ana",
+ "barbara",
+ "lucas",
+ "Sthefanie",
+ "beatriz",
+  "eduarda",
+ "matheus",
+ "carlos",
+ "evilyn",
+ "joao",
+ "maria",
+ "mileni",
+ "julia",
+ "thomas",
+ "gabriel",
+ "gustavo",
+ "igor",
+ "samantha",
+ "alison",
+ "pedro",
+ ];
+
+const sala1 = alunos.slice(0, 10);
+const sala2 = alunos.slice(10);
+
+
+console.log(sala1);
+console.log(sala2);
