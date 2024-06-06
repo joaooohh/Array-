545b4ece2f49941f156b28302d5a18e29d865b1e
joaooohh <140817126+joaooohh@users.noreply.github.com>
2024-06-06 11:24:05 -0300

    Add files via upload

---
+++ 9-remover-meio-splice.js
@@ -0,0 +1,13 @@
+const alunos = ['joão', 'ana', 'caio', 'lara', 'marjorie', 'leo', 'beatriz']
+
+
+
+
+
+
+
+alunos.slice(1, 1)
+console.log(alunos)
+
+alunos.splice(1,2,"rodrigo") // permite um terceiro parâmetro
+console.log(alunos)
