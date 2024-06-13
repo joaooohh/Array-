6ebfcbe06b8934e1101b09bc112688587afb5a2e
joaooohh <140817126+joaooohh@users.noreply.github.com>
2024-06-13 11:32:59 -0300

    Add files via upload

---
+++ 13-duas-dimensões.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+const medias = [10, 8, 7.5, 9]
+
+const listaDeAlunosEmedias = [alunos, medias]
+
+console.log(`O aluno da posição 1 da lista de alunos é: ${
+listaDeAlunosEmedias[0][1]}. A nota dessa aluna é ${listaDeAlunosEmedias[1][1]}`)
---
+++ 14-concatenando-array.js
@@ -0,0 +1,8 @@
+const nomes = ["Ana", "juliana", "Leonardo"];
+const idades = [30, 35, 28]; 
+const faculdade = [false, true, true];
+const funcionario = [nomes, idades, faculdade];
+if (funcionarios[2][0] === false){
+    console.log(`A funcionária ${funcionarios[0][0]}, possui ${funcionarios[1][0]}
+anos e não possui faculdade`)
+}
